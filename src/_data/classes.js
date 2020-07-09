5f21f55b327f4845eb62d132183ed3930f701cd0
Stetson Ramey <stetson.ramey@gmail.com>
2020-07-09 07:34:48 -0700

    finished up style guide
    mostly have it working.  would like to change the main content so it grows with screen size.

--- src/_data/classes.js
+++ src/_data/classes.js
@@ -3,10 +3,11 @@ module.exports = {
   html: 'font-normal font-sans leading-relaxed antialiased',
   body:
     'bg-bodybg text-bodytext text-lg md:text-xl overflow-x-hidden flex flex-col min-h-screen',
+  bodyGuide: 'lg:grid lg:grid-cols-2 lg:gap-4',
   navbar:
     'sticky top-0 z-10 bg-bodybg text-bodytext border-b border-cool-gray-100 dark:border-cool-gray-800 px-4 tracking-wider',
   main: 'flex flex-col flex-grow',
   '2Col': 'grid-cols-2 col-gap-4',
   contact_button: 'btn btn-primary text-white dark:text-cool-gray-900',
-  footer: 'border-t border-cool-gray-100 dark:border-gray-700 bg-muted',
+  footer: '',
 };
