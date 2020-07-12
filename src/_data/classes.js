fa7fdb7b8e7b9938d0140b859d1357f5a1027191
Stetson Ramey <stetson.ramey@gmail.com>
2020-07-12 08:11:18 -0700

    fixes
    fixed nav/footer on style page
    couple quick things on home page

--- src/_data/classes.js
+++ src/_data/classes.js
@@ -3,7 +3,7 @@ module.exports = {
   html: 'font-normal font-sans leading-relaxed antialiased',
   body:
     'bg-bodybg text-bodytext text-lg md:text-xl overflow-x-hidden flex flex-col min-h-screen',
-  bodyGuide: 'lg:grid lg:grid-cols-2 lg:gap-4',
+  bodyGuide: 'px-8 lg:grid lg:grid-cols-2 lg:gap-4',
   navbar:
     'w-full px-8 fixed top-0 left-0 shadow-nav z-10 bg-bodybg text-bodytext md:shadow-none md:static md:mt-8 md:mb-20 md:px-0',
   main: 'flex flex-col flex-grow',
