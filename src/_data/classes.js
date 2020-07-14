b3de105ffc4e939149bcb19964dfbfc60128d761
Stetson Ramey <stetson.ramey@gmail.com>
2020-07-14 13:50:35 -0700

    quick style fixes
    fixed nav bg
    fixed padding on style guide on mobile

--- src/_data/classes.js
+++ src/_data/classes.js
@@ -2,10 +2,10 @@ module.exports = {
   // Classes that will be added to different elements
   html: 'font-normal font-sans leading-relaxed antialiased',
   body:
-    'bg-bodybg text-bodytext text-lg md:text-xl overflow-x-hidden flex flex-col min-h-screen',
+    'bg-gray-100 text-bodytext text-lg md:text-xl overflow-x-hidden flex flex-col min-h-screen',
   bodyGuide: 'px-8 lg:grid lg:grid-cols-2 lg:gap-4',
   navbar:
-    'w-full px-8 fixed top-0 left-0 shadow-nav z-10 bg-bodybg text-bodytext md:shadow-none md:static md:mt-8 md:mb-20 md:px-0',
+    'w-full px-8 fixed top-0 left-0 shadow-nav z-10 bg-gray-100 text-bodytext md:shadow-none md:static md:mt-8 md:mb-20 md:px-0',
   main: 'flex flex-col flex-grow',
   '2Col': 'grid-cols-2 col-gap-4',
   contact_button: 'btn btn-primary text-white dark:text-cool-gray-900',
